91ab6e9c028c2e094e196da7c038cd202a5e5f50
amita.jethani <amitajethani@gmail.com>
2019-07-24 17:38:28 +0530

    Function

---
+++ Functions.js
@@ -0,0 +1,17 @@
+//function declaration
+function myFunct()
+{
+    //function definition
+    var print = "Hello dear";
+    console.log(print);
+}
+
+myFunct(); //function call 
+ function funcWithArgs(a,b)
+ {
+     console.log(a+b);
+ }
+ funcWithArgs(12,5);
+ funcWithArgs("Neha",5);
+ funcWithArgs(12,"Amita");
+
